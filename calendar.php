0cf1b8e7c9d7d8e7be19a165be87e0795d00d27d
Holló Gergő <36532796+ravenzjee@users.noreply.github.com>
2024-04-09 08:14:08 +0200

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -4,3 +4,9 @@
   if (!isset($_SESSION['adminuser'])) header("Location: login.php");
 
 ?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Áttekintő naptár | <?=SITE_NAME; ?></title>
