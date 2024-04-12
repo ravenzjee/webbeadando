7948441db7c359f4ecb9c0f4299cb24ed38bcbd7
Holló Gergő <36532796+ravenzjee@users.noreply.github.com>
2024-04-12 11:18:45 +0200

    Update pending.php

--- pending.php
+++ pending.php
@@ -46,7 +46,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Függőben lévő foglalások</h1>
+            <h1 class="m-0">Rólunk</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
           </div><!-- /.col -->
